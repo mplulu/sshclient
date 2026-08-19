bd29a280c0361f6b7f8fbd668c20edf755b55c4a
mplulu <hgia1234@gmail.com>
2026-08-19 17:07:37 +0700

    will not lock knownhost

--- known_host_flock_unix.go
+++ known_host_flock_unix.go
@@ -4,14 +4,15 @@ package sshclient
 
 import (
 	"os"
-
-	"golang.org/x/sys/unix"
 )
 
 func lockKnownHostsFile(f *os.File) error {
-	return unix.Flock(int(f.Fd()), unix.LOCK_EX)
+	// return unix.Flock(int(f.Fd()), unix.LOCK_EX)
+	// currently will not lock
+	return nil
 }
 
 func unlockKnownHostsFile(f *os.File) error {
-	return unix.Flock(int(f.Fd()), unix.LOCK_UN)
+	// return unix.Flock(int(f.Fd()), unix.LOCK_UN)
+	return nil
 }
